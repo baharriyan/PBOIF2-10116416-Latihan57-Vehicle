54bd7df1f441150f552ac5303613c3021d02ad9a
baharriyan <73172116+baharriyan@users.noreply.github.com>
2020-11-22 19:15:28 +0700

    Add files via upload

---
+++ PBOIF2-10116416-Latihan57-Vehicle/src/vehicle/VehicleMain.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vehicle;
+
+/**
+ *  
+ */
+public class VehicleMain {
+
+ /**
+ *Nama  : Riyan Bahar
+ *Kelas : PBO-02
+ *NIM   : 10116416
+ *Deskripso Program : program ini berisi program yang menampilkan konstruktor
+ *                    dari Vehicle
+ */
+    public static void main(String[] args) {
+        // TODO code application logic here
+          Bicycle bicycle = new Bicycle();
+        Skateboard skateboard = new Skateboard();
+    }
+    
+}
